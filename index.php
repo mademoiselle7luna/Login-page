0a99bbc18d2dc0d6cbde4fccd6d193796c33d72e
Kamila Świeżak <mademoiselle7luna@gmail.com>
2022-07-23 21:09:42 +0200

    Add most part of CSS.

--- index.php
+++ index.php
@@ -4,7 +4,10 @@ get_header();
 
 <div class="login-page">
 	<div class="container">
-		<div class="container__description"></div>
+		<div class="container__description">
+			<h1>Log in</h1>
+			<p>Log in with your data that you received from the photographer</p>
+		</div>
 		<form class="form">
 			<input class="form__email" type="text" placeholder="E-mail">
 			<input class="form__password" type=text" placeholder="Password">
@@ -18,3 +21,4 @@ get_header();
 
 <?php
 get_footer();
+?>
