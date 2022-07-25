17aedad5c825bed0b29c1170e805fb6de007cf71
Kamila Świeżak <mademoiselle7luna@gmail.com>
2022-07-25 18:12:11 +0200

    Fix BEM

--- index.php
+++ index.php
@@ -3,17 +3,17 @@ get_header();
 ?>
 
 <div class="login-page">
-	<div class="container">
-		<div class="container__description">
+	<div class="login-page__container">
+		<div class="login-page__container__description">
 			<h1>Log in</h1>
 			<p>Log in with your data that you received from the photographer</p>
 		</div>
-		<form class="form">
-			<input class="form__email" type="text" placeholder="E-mail">
-			<input class="form__password" type=text" placeholder="Password">
-			<div class="login-panel">
-				<a href="#" class="login-panel__reminder">Forgot password?</a>
-				<button class="login-panel__button">Log in</button>
+		<form class="login-page__form">
+			<input class="login-page__form__email" type="text" placeholder="E-mail">
+			<input class="login-page__form__password" type="text" placeholder="Password">
+			<div class="login-page__login-panel">
+				<a href="#" class="login-page__login-panel__reminder">Forgot password?</a>
+				<button class="login-page__login-panel__button">Log in</button>
 			</div>
 		</form>
 	</div>
